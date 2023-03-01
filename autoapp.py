08fbde46562c8c2b850f0b425433ccefedf461af
Dave Whiteland <davewhiteland@users.noreply.github.com>
2023-03-01 10:55:14 +0000

    simplify autoapp.py: it's now really just a redirecting stub

--- autoapp.py
+++ autoapp.py
@@ -1,9 +1,6 @@
 # -*- coding: utf-8 -*-
 """Create an application instance."""
 from buggy_race_server.app import create_app
-from buggy_race_server.utils import refresh_global_announcements
-from flask import request, redirect
-from buggy_race_server.config import ConfigSettingNames
 
 app = create_app()
 

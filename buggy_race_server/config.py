039d26d8c22fe6fb30da4355980746301fb08f4f
Dave Whiteland <davewhiteland@users.noreply.github.com>
2024-07-21 16:40:35 +0100

    add replay animations: v2.0.73 (#276)
    * remove duplicate word (typo) from config description
    
    * add basic animations for attack events in race player
    
    * check for 200 x 100 racetrack viewport in SVGs
    
    Anything else does affect the scaling of (for example) buggy "flag" rectangles
    so for now use JS to check and alert if it's wrong.
    
    * bump version to v2.0.73, with race animations

--- buggy_race_server/config.py
+++ buggy_race_server/config.py
@@ -32,7 +32,7 @@ from time import time
 #  When you do a release, [try to remember to] bump the release details here!
 # ----------------------------------------------------------------------------
 #
-MANUAL_LATEST_VERSION_IN_SOURCE = "v2.0.72"
+MANUAL_LATEST_VERSION_IN_SOURCE = "v2.0.73"
 #
 # ----------------------------------------------------------------------------
 
@@ -104,6 +104,9 @@ class ConfigSettingNames(Enum):
 
     # Path where static race assets (race player, etc) are found
     _RACE_ASSETS_PATH = auto()
+
+    # Path where static race player assets (images etc) are found
+    _RACE_ASSETS_IMAGES_PATH = auto()
 
     # Path where static racetrack assets (background images and path SVGs)
     # are found — might be a subdir of _RACE_ASSETS_PATH, but doesn't have
@@ -455,6 +458,7 @@ class ConfigSettings:
         ConfigSettingNames._PROJECT_TASKS_DIR_NAME.name: "project",
         ConfigSettingNames._PROJECT_TASKS_FILENAME.name: "tasks.md",
         ConfigSettingNames._RACE_ASSETS_PATH.name: path.join("buggy_race_server", "race", "assets"),
+        ConfigSettingNames._RACE_ASSETS_IMAGES_PATH.name: path.join("buggy_race_server", "race", "assets", "img"),
         ConfigSettingNames._RACE_ASSETS_RACETRACK_PATH.name: path.join("buggy_race_server", "race", "assets", "tracks"),
         ConfigSettingNames._SETUP_STATUS.name: 1, # by default, we're setting up!
         ConfigSettingNames._TASK_LIST_GENERATED_DATETIME.name: "",
@@ -1018,7 +1022,7 @@ class ConfigSettings:
           store the URL correctly when you update the race. If you're running
           an especially large project or little server, there may be efficiency
           benefits in _not_ using the database, but otherwise it's simplest to
-          to choose `Yes`. """,
+          choose `Yes`. """,
 
         ConfigSettingNames.IS_STORING_STUDENT_TASK_TEXTS.name:
           """Do you want students to be able to record text on this server
@@ -1552,6 +1556,7 @@ class ConfigSettings:
         ConfigSettingNames._EDITOR_REPO_DIR_NAME.name,
         ConfigSettingNames._EDITOR_ZIP_GENERATED_DATETIME.name,
         ConfigSettingNames._RACE_ASSETS_PATH.name,
+        ConfigSettingNames._RACE_ASSETS_IMAGES_PATH.name,
         ConfigSettingNames._RACE_ASSETS_RACETRACK_PATH.name,
         ConfigSettingNames._PUBLISHED_PATH.name,
         ConfigSettingNames._SETUP_STATUS.name,

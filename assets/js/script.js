37ccebf088b28017bdb5d33af397865640a846ea
Dave Whiteland <davewhiteland@users.noreply.github.com>
2026-06-10 00:03:13 +0100

    fix comparison error with parseInt

--- assets/js/script.js
+++ assets/js/script.js
@@ -680,15 +680,14 @@ $(function() {
   if (word_count_slider){
     let tt_matrix = document.getElementById("all-student-texts");
     let tt_cells = tt_matrix.getElementsByClassName("tt-cell");
-    console.log("FIXME found " + tt_cells.length + " tt_cells");
     let current_value_span = document.getElementById("wc-current-threshold");
     word_count_slider.addEventListener("change", function(e){
-      let threshold_value = word_count_slider.value;
+      let threshold_value = parseInt(word_count_slider.value);
       current_value_span.innerText = threshold_value;
       for (let tt_cell of tt_cells) {
         let wc = tt_cell.dataset.wc;
         if (wc !== undefined) {
-          if (wc < threshold_value) {
+          if (parseInt(wc) < threshold_value) {
             tt_cell.classList.add("bg-warning");
             tt_cell.classList.remove("bg-success");
           } else {

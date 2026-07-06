0bd989d2d905694020e58d8417ff6f94bc53a406
Dave Whiteland <davewhiteland@users.noreply.github.com>
2026-07-06 14:04:27 +0100

    refactored buggy upload handling to fix some API flash leaks, and duplicate definitions
    Uses's json's object_pairs_hook to detect, and report on, duplicate property
    definitions: they are not necessarily bad JSON, but they don't do what the
    student thinks they do: see #315.
    
    There were also some flash messages being set with the API, so tidied those up:
    this could still be better since it's a bit horrible that there are all these
    conditionals in there at the moment, but this is OK for now.

--- buggy_race_server/buggy/views.py
+++ buggy_race_server/buggy/views.py
@@ -28,142 +28,218 @@ blueprint = Blueprint("buggy", __name__, url_prefix="/buggy", static_folder="../
 
 # if is_api responds differently
 def handle_uploaded_json(form, user, is_api=False):
-  # is_api validation doesn't work:
-  # so we're checking buggy_json field explicitly before getting here
-  if (form.is_submitted() and form.validate()) or is_api:
-    user.latest_json = form.buggy_json.data
-    user.uploaded_at = datetime.now(timezone.utc)
-    user.save()
-    try:
-      dirty_buggy_data = json.loads(form.buggy_json.data)
-    except json.decoder.JSONDecodeError as e:
-      if is_api:
-        return {"error": "Failed to parse JSON data"}
-      else:
-        flash("Failed to parse JSON data", "danger")
-        flash(str(e), "warning")
-        flash("No data was accepted", "info")
-        return redirect(url_for("user.submit_buggy_data"))
-    clean_buggy_data = {}
-    word_too = ""
-    is_multi_buggy_suspected = type(dirty_buggy_data) == list
-    for key in dirty_buggy_data:
-      key_type = type(key)
-      if key_type != str:
-          flash(f"Make sure your buggy JSON only contains keys which are strings: ignoring {key_type}", "warning")
-          is_multi_buggy_suspected = is_multi_buggy_suspected or key_type in (dict, list)
-      elif key == 'id': # user's buggy's id becomes buggy_id here
-          try:
-            clean_buggy_data['buggy_id'] = int(dirty_buggy_data[key])
-          except ValueError:
-            if not is_api:
-              flash("Value for id was ignored because it wasn't an integer", "warning")
-      elif key in Buggy.DEFAULTS:
-        if Buggy.DEFAULTS[key] == False and isinstance(Buggy.DEFAULTS[key], bool):
-            if isinstance(dirty_buggy_data[key], bool):
-              clean_buggy_data[key] = int(dirty_buggy_data[key])
-            else:
-              dirty_buggy_data[key] = str(dirty_buggy_data[key]).strip().lower()
-              was_ok_boolean = True
-              if dirty_buggy_data[key] == 'true':
-                clean_buggy_data[key] = True
-              elif dirty_buggy_data[key] == 'false':
-                clean_buggy_data[key] = False
-              elif dirty_buggy_data[key] == "1":
-                clean_buggy_data[key] = True
-              elif dirty_buggy_data[key] == "0":
-                clean_buggy_data[key] = False
-              else:
-                was_ok_boolean = False
-                if not is_api:
-                  flash(f"Value for {key} was ignored because it wasn't true or false", "warning")
-              if not is_api and was_ok_boolean:
-                flash(f"Value for {key} wasn't a JSON boolean, but OK: \"{dirty_buggy_data[key]}\" accepted as {str(clean_buggy_data[key]).lower()}", "info")
-        elif isinstance(Buggy.DEFAULTS[key], int):
-          try:
-            clean_buggy_data[key] = int(dirty_buggy_data[key])
-          except ValueError:
-            if not is_api:
-              flash(f"Value for {key} was ignored because it wasn't an integer", "warning")
-        else:
-          dirty_buggy_data[key] = str(dirty_buggy_data[key]).strip().lower()
-          s = "#" if dirty_buggy_data[key].startswith("#") else ""
-          STRING_CHARS = string.digits + string.ascii_letters
-          s += "".join(c for c in dirty_buggy_data[key] if c in STRING_CHARS)
-          if s == "":
-              if not is_api:
-                  flash(f"Value for {key} was ignored because it didn't have any alphanumeric characters in it", "warning")
-          else:
-            # check lengths:
-            if max_str_len := Buggy.STRING_COL_LENGTH.get(key):
-                if len(s) > max_str_len:
-                  s = s[:max_str_len]
-                  if not is_api:
-                    flash(f"Value for {key} was truncated to {max_str_len} characters", "warning")
-            if key in Buggy.GAME_DATA:
-                if s not in Buggy.GAME_DATA[key]:
-                    flash(f"Value for {key} was ignored because \"{s}\" is not a valid choice", "warning")
-                else:
-                    clean_buggy_data[key] = s # it's a value in a list 
-            else:
-                clean_buggy_data[key] = s # free-form string (e.g., flag_color)
-      else:
-        if not is_api:
-          flash("Unrecognised setting \"{}\" was ignored {}".format(key, word_too), "warning")
-          word_too = "too"
-    if is_multi_buggy_suspected:
-        flash("Maybe you tried to upload more than one buggy? You can only upload a single JSON object here!", "danger")
-    qty_defaults = 0
-    qty_explicits = 0
-    for field_name in Buggy.DEFAULTS:
-        if field_name in clean_buggy_data:
-            qty_explicits += 1
-        else:
-            if field_name == "flag_color":
-                clean_buggy_data[field_name] = current_app.config[
-                    ConfigSettingNames.DEFAULT_FLAG_COLOR.name
-                ]
-            else:
-                clean_buggy_data[field_name] = Buggy.DEFAULTS[field_name]
-            qty_defaults += 1
-    if qty_explicits == 0:
-        msg = "Nothing to change: no buggy settings were found in the uploaded data"
-        if is_api:
-            return {"error": msg}
-        flash(msg, "danger")
-    else:
-        (s, was) = ("s", "were") if qty_explicits > 1 else ("", "was")
-        flash(f"{qty_explicits} setting{s} {was} specified", "info")
-        if qty_defaults > 0:
-            if not is_api:
-                (s, was) = ("s", "were") if qty_defaults > 1 else ("", "was")
-                flash(f"{qty_defaults} setting{s} {was} not specified and got default value{s} instead", "info")
-        if 'buggy_id' not in clean_buggy_data:
-            clean_buggy_data['buggy_id'] = 1 # TODO not sure
-        users_buggy = Buggy.query.filter_by(user_id=user.id).first()
-        if users_buggy is None:
-            Buggy.create(user_id = user.id, **clean_buggy_data)
+    # is_api validation doesn't work:
+    # so we're checking buggy_json field explicitly before getting here
+    if (form.is_submitted() and form.validate()) or is_api:
+        user.latest_json = form.buggy_json.data
+        user.uploaded_at = datetime.now(timezone.utc)
+        user.save()
+
+        duplicate_properties_reported = set()
+        def _check_for_duplicate_properties(incoming_data_pairs):
+            dedupped_dic = {}
+            for (key, value) in incoming_data_pairs:
+                if not is_api and key in dedupped_dic:
+                    if key not in duplicate_properties_reported:
+                        flash(
+                            f"JSON contains multiple definitions for {key}, "
+                            "ignoring all but last value",
+                            "warning"
+                        )
+                        duplicate_properties_reported.add(key)
+                dedupped_dic[key] = value
+            return dedupped_dic
+
+        try:
+            dirty_buggy_data = json.loads(
+                form.buggy_json.data,
+                object_pairs_hook=_check_for_duplicate_properties
+            )
+        except json.decoder.JSONDecodeError as e:
             if is_api:
-                return {"ok": "buggy created OK"}
-            flash("JSON data for your racing buggy saved OK", "success")
+                return {"error": "Failed to parse JSON data"}
+            else:
+                flash("Failed to parse JSON data", "danger")
+                flash(str(e), "warning")
+                flash("No data was accepted", "info")
+                return redirect(url_for("user.submit_buggy_data"))
+        clean_buggy_data = {}
+        word_too = ""
+        if type(dirty_buggy_data) == list:
+            if is_api:
+                return {"error": "JSON is a list, but should be a JSON object"}
+            flash(
+                "Your JSON should be a single JSON object, with property names, not a list. "
+                "Maybe you tried to upload more than one buggy?",
+                "danger"
+            )
+        elif type(dirty_buggy_data) != dict:
+            if dirty_buggy_data is None:
+                if is_api:
+                    return {"error": "JSON is null, but should be a JSON object"}
+                flash(
+                    "Your JSON should be a single JSON object, with property names, not \"null\". "
+                    "This doesn't describe a buggy!",
+                    "danger"
+                )
+            else:
+                if is_api:
+                    return {"error": "JSON is a value, but should be a JSON object"}
+                flash(
+                    "Your JSON should be a single JSON object, with property names, not a value. "
+                    "This doesn't describe a buggy!",
+                    "danger"
+                )
+        else: # it's a dict, so we can iterate through the keys
+            for key in dirty_buggy_data:
+                key_type = type(key)
+                if key_type != str:
+                    if not is_api:
+                        flash(
+                            "Make sure your buggy JSON only contains properties which are strings: "
+                            f"ignoring {key_type}",
+                            "warning"
+                        )
+                elif key == 'id': # user's buggy's id becomes buggy_id here
+                    try:
+                        clean_buggy_data['buggy_id'] = int(dirty_buggy_data[key])
+                    except ValueError:
+                        if not is_api:
+                            flash(
+                                "Value for id was ignored because it wasn't an integer",
+                                "warning"
+                            )
+                elif key in Buggy.DEFAULTS:
+                    if Buggy.DEFAULTS[key] == False and isinstance(Buggy.DEFAULTS[key], bool):
+                        if isinstance(dirty_buggy_data[key], bool):
+                            clean_buggy_data[key] = int(dirty_buggy_data[key])
+                        else:
+                            dirty_buggy_data[key] = str(dirty_buggy_data[key]).strip().lower()
+                            was_ok_boolean = True
+                            if dirty_buggy_data[key] == 'true':
+                                clean_buggy_data[key] = True
+                            elif dirty_buggy_data[key] == 'false':
+                                clean_buggy_data[key] = False
+                            elif dirty_buggy_data[key] == "1":
+                                clean_buggy_data[key] = True
+                            elif dirty_buggy_data[key] == "0":
+                                clean_buggy_data[key] = False
+                            else:
+                                was_ok_boolean = False
+                                if not is_api:
+                                    flash(
+                                        f"Value for {key} was ignored because it wasn't true or false",
+                                        "warning"
+                                    )
+                            if not is_api and was_ok_boolean:
+                                flash(
+                                    f"Value for {key} wasn't a JSON boolean, but OK: "
+                                    f"\"{dirty_buggy_data[key]}\" accepted as {str(clean_buggy_data[key]).lower()}",
+                                    "info"
+                                )
+                    elif isinstance(Buggy.DEFAULTS[key], int):
+                        try:
+                            clean_buggy_data[key] = int(dirty_buggy_data[key])
+                        except ValueError:
+                            if not is_api:
+                                flash(
+                                    f"Value for {key} was ignored because it wasn't an integer",
+                                    "warning"
+                                )
+                    else:
+                        dirty_buggy_data[key] = str(dirty_buggy_data[key]).strip().lower()
+                        s = "#" if dirty_buggy_data[key].startswith("#") else ""
+                        STRING_CHARS = string.digits + string.ascii_letters
+                        s += "".join(c for c in dirty_buggy_data[key] if c in STRING_CHARS)
+                        if s == "":
+                            if not is_api:
+                                flash(
+                                    f"Value for {key} was ignored because it didn't have "
+                                    "any alphanumeric characters in it",
+                                    "warning"
+                                )
+                        else:
+                            # check lengths:
+                            if max_str_len := Buggy.STRING_COL_LENGTH.get(key):
+                                if len(s) > max_str_len:
+                                    s = s[:max_str_len]
+                                    if not is_api:
+                                        flash(
+                                            f"Value for {key} was truncated to {max_str_len} characters",
+                                            "warning"
+                                        )
+                        if key in Buggy.GAME_DATA:
+                            if s not in Buggy.GAME_DATA[key]:
+                                if not is_api:
+                                    flash(
+                                        f"Value for {key} was ignored because \"{s}\" is not a valid choice"
+                                        "warning"
+                                    )
+                            else:
+                                clean_buggy_data[key] = s # it's a value in a list
+                        else:
+                            clean_buggy_data[key] = s # free-form string (e.g., flag_color)
+                else:
+                    if not is_api:
+                        flash(
+                            f"Unrecognised setting \"{key}\" was ignored {word_too}",
+                            "warning"
+                        )
+                        word_too = "too"
+        qty_defaults = 0
+        qty_explicits = 0
+        for field_name in Buggy.DEFAULTS:
+            if field_name in clean_buggy_data:
+                qty_explicits += 1
+            else:
+                if field_name == "flag_color":
+                    clean_buggy_data[field_name] = current_app.config[
+                        ConfigSettingNames.DEFAULT_FLAG_COLOR.name
+                    ]
+                else:
+                    clean_buggy_data[field_name] = Buggy.DEFAULTS[field_name]
+                qty_defaults += 1
+        if qty_explicits == 0:
+            msg = "Nothing to change: no buggy settings were found in the uploaded data"
+            if is_api:
+                return {"error": msg}
+            flash(msg, "danger")
         else:
-          for field_name in clean_buggy_data:
-              setattr(users_buggy, field_name, clean_buggy_data[field_name])
-          users_buggy.save()
-          if not is_api:
-              flash("JSON data for your racing buggy updated OK", "success")
-        if is_api:
-            return {"ok": "buggy updated OK"}
-        if user == current_user:
-            return redirect(url_for("buggy.show_own_buggy"))
-        else:
-            return redirect(url_for("admin.show_buggy", user_id=user.username))
-  else:
-      flash_errors(form)
-  if is_api:
-      return {"error": "buggy data is missing"}
-  else:
-      return render_template("user/submit_buggy_data.html", form=form)
+            (s, was) = ("s", "were") if qty_explicits > 1 else ("", "was")
+            if not is_api:
+                flash(f"{qty_explicits} setting{s} {was} specified", "info")
+            if qty_defaults > 0:
+                if not is_api:
+                    (s, was) = ("s", "were") if qty_defaults > 1 else ("", "was")
+                    flash(
+                        f"{qty_defaults} setting{s} {was} not specified and got "
+                        f"default value{s} instead",
+                        "info"
+                    )
+            if 'buggy_id' not in clean_buggy_data:
+                clean_buggy_data['buggy_id'] = 1 # force this as a default
+            users_buggy = Buggy.query.filter_by(user_id=user.id).first()
+            if users_buggy is None:
+                Buggy.create(user_id = user.id, **clean_buggy_data)
+                if is_api:
+                    return {"ok": "buggy created OK"}
+                flash("JSON data for your racing buggy saved OK", "success")
+            else:
+                for field_name in clean_buggy_data:
+                    setattr(users_buggy, field_name, clean_buggy_data[field_name])
+                users_buggy.save()
+                if is_api:
+                    return {"ok": "buggy updated OK"}
+                flash("JSON data for your racing buggy updated OK", "success")
+            if user == current_user:
+                return redirect(url_for("buggy.show_own_buggy"))
+            else:
+                return redirect(url_for("admin.show_buggy", user_id=user.username))
+    else:
+        flash_errors(form)
+    if is_api:
+        return {"error": "buggy data is missing"}
+    return render_template("user/submit_buggy_data.html", form=form)
 
 @blueprint.route("/json", methods=["POST"], strict_slashes=False)
 @login_required
